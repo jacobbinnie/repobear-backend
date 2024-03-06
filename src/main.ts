99993ac6837ff928c403fcfee381299bb9ef8ca3
jacobbinnie <hello@jacobbinnie.com>
2024-03-06 12:49:44 -0500

    feat: add swagger to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Repobear Backend')
+    .setDescription('The backend infra for Repobear')
+    .setVersion('1.0')
+    .addTag('backend')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3232);
 }
 bootstrap();
