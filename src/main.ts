a1a9887e0837453de0aa2f0155c1540943e63540
jacobbinnie <hello@jacobbinnie.com>
2024-03-17 17:08:18 -0400

    feat: tidy up

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
-
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
